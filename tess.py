82e2a9a3403316ff4c9c458b5c61bf8866a0a7f5
gzancewicz <gzancewicz@gmail.com>
2021-10-14 11:41:52 -0500

    Fixed bug in argument to urlopen

--- tess.py
+++ tess.py
@@ -5,7 +5,7 @@ import pytesseract
 import urllib.request
 import numpy as np
 
-url_response = urllib.request.urlopen(argv[1])
+url_response = urllib.request.urlopen(sys.argv[1])
 img = cv2.imdecode(np.array(bytearray(url_response.read()), dtype=np.uint8), -1)
 
 text = pytesseract.image_to_string(img)
